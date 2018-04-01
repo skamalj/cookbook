4a4c1c953f7a97685043a03e105f0dd5e7babcc5
Kamal <skamalj@gmail.com>
2018-04-01 22:58:37 +0530

    Install Apache

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,9 @@
 Chef::Log.info("******Installing Apache Webserver **********")
 package 'Install Apache' do
-	package 'apache2'
+	package_name 'apache2'
+end
+
+service 'Start Apache' do
+        service_name 'apache2'
+	action :enable, :start
 end
